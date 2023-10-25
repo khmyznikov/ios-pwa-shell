c05ac2edb0543935b211c72472f9e52efaeb6004
Gleb Khmyznikov <gleb.khmyznikov@gmail.com>
2023-10-25 21:19:03 +0200

    Try to fix gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/ios-pwa-shell",
   build: {
     sourcemap: true,
     assetsDir: "code",
